0dccd8c86d072bcae03792c62ed3a675ab7963e1
Alexandre <nawtz_sky@hotmail.com>
2025-03-17 19:54:43 +0000

    added all menu options test into single test and fetches options from file

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -4,12 +4,6 @@ import LoginPage from '../pages/LoginPage';
 describe("Homepage Navigation Tests", () => {
   const home = new HomePage();
   const login = new LoginPage();
-  
-  const menuItems = [
-    { label: "About", expectedUrl: "/about" },
-    { label: "Documentation", expectedUrl: "/documentation" },
-    { label: "Terms", expectedUrl: "/terms" }
-  ];
 
   beforeEach(() => {
     home.visit();
@@ -21,12 +15,13 @@ describe("Homepage Navigation Tests", () => {
     login.getPasswordField().should("be.visible");
   });
 
-  menuItems.forEach(({ label, expectedUrl }) => {
-    it(`should navigate to ${label} page`, () => {
-      home.getNavOption(label).click();
-
-      cy.url().should("include", expectedUrl);
-      home.getNavOption(label).should("be.visible");
+  it("should navigate to each menu item", () => {
+    cy.fixture("navigationOptions").then((data) => {
+      data.menuItems.forEach(({ label, expectedUrl }) => {
+        home.getNavOption(label).click();
+        cy.url().should("include", expectedUrl);
+        home.getNavOption(label).should("be.visible");
+      });
     });
   });
 });
