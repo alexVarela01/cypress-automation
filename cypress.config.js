95147f0deee328a56e140d9c7e79115ad7d0fe91
Alexandre <nawtz_sky@hotmail.com>
2025-03-17 19:57:53 +0000

    increased timeout

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
   projectId: 'st6x1v',
   e2e: {
     baseUrl: 'https://monetis-delta.vercel.app',
